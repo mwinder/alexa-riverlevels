e30fdfb0c18071c32d9283c470bfe101e8890f72
Matthew Winder <mwinder@users.noreply.github.com>
2017-07-19 12:36:41 +0100

    Slimmed down GetResources and accepted some Resharper suggesttions

--- RiverLevelsSkill/Function.cs
+++ RiverLevelsSkill/Function.cs
@@ -23,9 +23,10 @@ namespace RiverLevelsSkill
         /// <returns></returns>
         public SkillResponse Handler(SkillRequest input, ILambdaContext context)
         {
-            var response = new SkillResponse();
-            response.Response = new ResponseBody();
-            response.Response.ShouldEndSession = false;
+            var response = new SkillResponse
+            {
+                Response = new ResponseBody { ShouldEndSession = false }
+            };
             IOutputSpeech innerResponse = null;
             var log = context.Logger;
             log.LogLine($"Skill Request Object:");
@@ -95,31 +96,32 @@ namespace RiverLevelsSkill
             return resource.Facts[r.Next(resource.Facts.Count)];
         }
 
-        private List<FactResource> GetResources()
+        private static IEnumerable<FactResource> GetResources()
         {
-            List<FactResource> resources = new List<FactResource>();
-            FactResource enUSResource = new FactResource("en-US");
-            enUSResource.SkillName = "American Space Facts";
-            enUSResource.GetFactMessage = "Here's your fact: ";
-            enUSResource.HelpMessage = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
-            enUSResource.HelpReprompt = "What can I help you with?";
-            enUSResource.StopMessage = "Goodbye!";
-            enUSResource.Facts.Add("A year on Mercury is just 88 days long.");
-            enUSResource.Facts.Add("Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.");
-            enUSResource.Facts.Add("Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.");
-            enUSResource.Facts.Add("On Mars, the Sun appears about half the size as it does on Earth.");
-            enUSResource.Facts.Add("Earth is the only planet not named after a god.");
-            enUSResource.Facts.Add("Jupiter has the shortest day of all the planets.");
-            enUSResource.Facts.Add("The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.");
-            enUSResource.Facts.Add("The Sun contains 99.86% of the mass in the Solar System.");
-            enUSResource.Facts.Add("The Sun is an almost perfect sphere.");
-            enUSResource.Facts.Add("A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.");
-            enUSResource.Facts.Add("Saturn radiates two and a half times more energy into space than it receives from the sun.");
-            enUSResource.Facts.Add("The temperature inside the Sun can reach 15 million degrees Celsius.");
-            enUSResource.Facts.Add("The Moon is moving approximately 3.8 cm away from our planet every year.");
-
-            resources.Add(enUSResource);
-            return resources;
+            yield return new FactResource("en-US")
+            {
+                SkillName = "American Space Facts",
+                GetFactMessage = "Here's your fact: ",
+                HelpMessage = "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+                HelpReprompt = "What can I help you with?",
+                StopMessage = "Goodbye!",
+                Facts = new List<string>
+                {
+                    "A year on Mercury is just 88 days long.",
+                    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
+                    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
+                    "On Mars, the Sun appears about half the size as it does on Earth.",
+                    "Earth is the only planet not named after a god.",
+                    "Jupiter has the shortest day of all the planets.",
+                    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
+                    "The Sun contains 99.86% of the mass in the Solar System.",
+                    "The Sun is an almost perfect sphere.",
+                    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
+                    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
+                    "The temperature inside the Sun can reach 15 million degrees Celsius.",
+                    "The Moon is moving approximately 3.8 cm away from our planet every year.",
+                },
+            };
         }
     }
 
